f191caf6d12631af95739bf583060063fa56972e
Douman <douman@gmx.se>
2021-12-22 15:54:37 +0900

    0.2.0: Refactoring to simplify and remove deps

--- src/lib.rs
+++ src/lib.rs
@@ -3,23 +3,10 @@
 //! Implementation uses type erased values with type as index.
 //! Due to limitation of `TypeId` only types without non-static references are supported. (in future it can be changed)
 //!
-//! ## Type erasure
-//!
-//! Each inserted value is stored on heap, with type erased pointer, using type as key.
-//! When value is retrieved, type information is used as key and pointer is casted to corresponding the type.
-//! This is safe, because Rust allows cast back and forth between pointers as long as the pointer actually points to the type (which is the case).
-//!
-//! Static references are allowed, but in current implementation are stored on heap.
-//! It might be changed in future.
-//!
 //! ## Hash implementation
 //!
 //! The map uses simplified `Hasher` that relies on fact that `TypeId` produces unique values only.
 //! In fact there is no hashing under hood, and type's id is returned as it is.
-//!
-//! ## Requirements:
-//!
-//! - `alloc` with enabled global allocator
 //!
 //! ## Usage
 //!
@@ -40,38 +27,37 @@
 //! ```
 
 #![warn(missing_docs)]
-#![no_std]
-
-extern crate alloc;
-
-use smart_ptr::unique::Unique;
 
 use core::any::TypeId;
-use core::{mem};
 
 mod hash;
 
-type HashMap = indexmap::IndexMap<TypeId, Unique<u8, fn (*mut u8)>, hash::UniqueHasherBuilder>;
-
-fn boxed_deleter<T>(ptr: *mut u8) {
-    unsafe {
-        alloc::boxed::Box::from_raw(ptr as *mut T);
-    }
+#[cfg(not(debug_assertions))]
+macro_rules! unreach {
+    () => ({
+        unsafe {
+            core::hint::unreachable_unchecked();
+        }
+    })
 }
+
+#[cfg(debug_assertions)]
+macro_rules! unreach {
+    () => ({
+        unreachable!()
+    })
+}
+
+type HashMap = std::collections::HashMap<TypeId, Box<dyn core::any::Any + Send + Sync>, hash::UniqueHasherBuilder>;
 
 ///Type-safe store, indexed by types.
 pub struct TypeMap {
     inner: HashMap,
 }
 
-///Type's map `Key` trait.
-///
-///Due to limitations of `TypeId`, safe code allows only to insert static types (i.e. types that doesn't contain non-static lifetimes)
-pub trait Key: 'static {}
-//TODO: It should be fairly trivial to replace TypeId with custom impl that allows non-statics, but
-//I doubt it is good idea
-
-impl<T: 'static> Key for T {}
+///Valid type for `TypeMap`
+pub trait Type: 'static + Send + Sync {}
+impl<T: 'static + Send + Sync> Type for T {}
 
 impl TypeMap {
     #[inline]
@@ -108,83 +94,94 @@ impl TypeMap {
 
     #[inline]
     ///Returns whether element is present in the map.
-    pub fn has<T: Key>(&self) -> bool {
+    pub fn has<T: Type>(&self) -> bool {
         self.inner.contains_key(&TypeId::of::<T>())
     }
 
     #[inline]
     ///Returns whether element is present in the map.
-    pub fn contains_key<T: Key>(&self) -> bool {
+    pub fn contains_key<T: Type>(&self) -> bool {
         self.inner.contains_key(&TypeId::of::<T>())
     }
 
     #[inline]
     ///Access element in the map, returning reference to it, if present
-    pub fn get<T: Key>(&self) -> Option<&T> {
-        self.inner.get(&TypeId::of::<T>()).map(|ptr| unsafe {
-            &*ptr.const_cast::<T>()
-        })
+    pub fn get<T: Type>(&self) -> Option<&T> {
+        match self.inner.get(&TypeId::of::<T>()) {
+            Some(ptr) => match ptr.downcast_ref() {
+                Some(res) => Some(res),
+                None => unreach!(),
+            },
+            None => None
+        }
     }
 
     #[inline]
     ///Access element in the map, returning mutable reference to it, if present
-    pub fn get_mut<T: Key>(&mut self) -> Option<&mut T> {
-        self.inner.get_mut(&TypeId::of::<T>()).map(|ptr| unsafe {
-            &mut *ptr.cast::<T>()
-        })
+    pub fn get_mut<T: Type>(&mut self) -> Option<&mut T> {
+        match self.inner.get_mut(&TypeId::of::<T>()) {
+            Some(ptr) => match ptr.downcast_mut() {
+                Some(res) => Some(res),
+                None => unreach!(),
+            },
+            None => None
+        }
     }
 
     #[inline]
     ///Access element in the map, if not present, constructs it using default value.
-    pub fn get_or_default<T: Key + Default>(&mut self) -> &mut T {
-        match self.inner.entry(TypeId::of::<T>()) {
-            indexmap::map::Entry::Occupied(mut occupied) => unsafe {
-                &mut *occupied.get_mut().cast::<T>()
-            },
-            indexmap::map::Entry::Vacant(vacant) => {
-                let deleter: fn (*mut u8) = boxed_deleter::<T>;
-                let ptr = unsafe {
-                    Unique::from_ptr_unchecked(alloc::boxed::Box::into_raw(alloc::boxed::Box::new(T::default())) as *mut u8, deleter)
-                };
+    pub fn get_or_default<T: Type + Default>(&mut self) -> &mut T {
+        use std::collections::hash_map::Entry;
 
-                let ptr = vacant.insert(ptr);
-                unsafe {
-                    &mut *ptr.cast::<T>()
+        match self.inner.entry(TypeId::of::<T>()) {
+            Entry::Occupied(occupied) => {
+                match occupied.into_mut().downcast_mut() {
+                    Some(res) => res,
+                    None => unreach!(),
+                }
+            },
+            Entry::Vacant(vacant) => {
+                let ptr = vacant.insert(Box::new(T::default()));
+                match ptr.downcast_mut() {
+                    Some(res) => res,
+                    None => unreach!(),
                 }
             }
         }
     }
 
-    ///Insert element inside the map, returning old one if any
+    ///Insert element inside the map, returning heap-allocated old one if any
     ///
     ///## Note
     ///
     ///Be careful when inserting without explicitly specifying type.
     ///Some special types like function pointers are impossible to infer as non-anonymous type.
     ///You should manually specify type when in doubt.
-    pub fn insert<T: Key>(&mut self, mut value: T) -> Option<T> {
-        match self.inner.entry(TypeId::of::<T>()) {
-            indexmap::map::Entry::Occupied(mut occupied) => {
-                mem::swap(unsafe { &mut *occupied.get_mut().cast::<T>() }, &mut value);
-                Some(value)
-            },
-            indexmap::map::Entry::Vacant(vacant) => {
-                let deleter: fn (*mut u8) = boxed_deleter::<T>;
-                let ptr = unsafe {
-                    Unique::from_ptr_unchecked(alloc::boxed::Box::into_raw(alloc::boxed::Box::new(value)) as *mut u8, deleter)
-                };
+    pub fn insert<T: Type>(&mut self, value: T) -> Option<Box<T>> {
+        use std::collections::hash_map::Entry;
 
-                vacant.insert(ptr);
+        match self.inner.entry(TypeId::of::<T>()) {
+            Entry::Occupied(mut occupied) => {
+                let result = occupied.insert(Box::new(value));
+                match result.downcast() {
+                    Ok(result) => Some(result),
+                    Err(_) => unreach!()
+                }
+            },
+            Entry::Vacant(vacant) => {
+                vacant.insert(Box::new(value));
                 None
             }
         }
     }
 
-    ///Attempts to remove element from the map, returning `Some` if it is present
-    pub fn remove<T: Key>(&mut self) -> Option<T> {
-        self.inner.remove(&TypeId::of::<T>()).map(|ptr| unsafe {
-            let ptr = alloc::boxed::Box::from_raw(ptr.release().as_ptr() as *mut T);
-            *ptr
+    ///Attempts to remove element from the map, returning boxed `Some` if it is present.
+    pub fn remove<T: Type>(&mut self) -> Option<Box<T>> {
+        self.inner.remove(&TypeId::of::<T>()).map(|ptr| {
+            match ptr.downcast() {
+                Ok(result) => result,
+                Err(_) => unreach!()
+            }
         })
     }
 }
--- tests/type_map.rs
+++ tests/type_map.rs
@@ -7,22 +7,36 @@ fn check_type_map() {
     assert!(map.is_empty());
     assert_eq!(map.len(), 0);
 
+    assert!(map.insert("test").is_none());
+    assert_eq!(*map.insert("lolka").unwrap(), "test");
+    assert_eq!(*map.get::<&'static str>().unwrap(), "lolka");
+
+    assert!(map.insert::<fn ()>(check_type_map).is_none());
+    assert!(*map.get::<fn ()>().unwrap() == check_type_map);
+
     assert!(!map.has::<usize>());
     assert_eq!(*map.get_or_default::<usize>(), 0);
     *map.get_or_default::<usize>() = 5;
+    assert_eq!(*map.get_or_default::<usize>(), 5);
 
     assert_eq!(*map.get::<usize>().unwrap(), 5);
 
     assert!(!map.is_empty());
-    assert_eq!(map.len(), 1);
+    assert_eq!(map.len(), 3);
 
-    assert_eq!(map.remove::<usize>().unwrap(), 5);
+    assert_eq!(*map.remove::<usize>().unwrap(), 5);
+    assert_eq!(map.len(), 2);
+    assert_eq!(map.remove::<usize>(), None);
+
+    assert_eq!(*map.remove::<&'static str>().unwrap(), "lolka");
+    assert_eq!(map.len(), 1);
+    assert_eq!(map.remove::<&'static str>(), None);
+
+    assert!(*map.remove::<fn ()>().unwrap() == check_type_map);
+    assert_eq!(map.len(), 0);
+    assert_eq!(map.remove::<fn ()>(), None);
 
     assert!(map.is_empty());
-    assert_eq!(map.len(), 0);
-
-    assert!(map.insert::<fn ()>(check_type_map).is_none());
-    assert!(*map.get::<fn ()>().unwrap() == check_type_map);
 
     map.clear();
     assert!(map.is_empty());
